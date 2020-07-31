fa3477c5b791e2ffd3778eeec7ff352db89ef277
w6o6n <47755670+w6o6n@users.noreply.github.com>
2020-07-31 23:32:11 +0900

    xdna2 ver.2.4. First release of this personal formula

---
+++ Formula/xdna2.rb
@@ -0,0 +1,36 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Xdna2 < Formula
+  desc "Convert DNA Strider (.xdna) files"
+  homepage "https://incenp.org/dvlpt/xdna2.html"
+  url "https://incenp.org/files/softs/xdna2/0.2/xdna2-0.2.4.tar.gz"
+  sha256 "3d75a154ad9249fe3eb535c703c6623a99d7b0e00da4652bcfdf79929ab49245"
+  license "GPL-3.0-only"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test xdna2`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
