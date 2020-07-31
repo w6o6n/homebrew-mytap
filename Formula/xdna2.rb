fa4aba80052468a77fb3de9cbe65096fb9eec2e2
w6o6n <47755670+w6o6n@users.noreply.github.com>
2020-08-01 05:13:10 +0900

    mod desc

--- Formula/xdna2.rb
+++ Formula/xdna2.rb
@@ -1,5 +1,5 @@
 class Xdna2 < Formula
-  desc "Convert DNA Strider (.xdna) files"
+  desc "Convert DNA Strider (.xdna) files to GenBank/FASTA format"
   homepage "https://incenp.org/dvlpt/xdna2.html"
   url "https://incenp.org/files/softs/xdna2/0.2/xdna2-0.2.4.tar.gz"
   sha256 "3d75a154ad9249fe3eb535c703c6623a99d7b0e00da4652bcfdf79929ab49245"
