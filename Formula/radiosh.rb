4650f5f9103c8940183f7f7cebf9eb2630857605
w6o6n <47755670+w6o6n@users.noreply.github.com>
2020-09-08 23:54:18 +0900

    radiosh,formula OK

--- Formula/radiosh.rb
+++ Formula/radiosh.rb
@@ -6,29 +6,14 @@ class Radiosh < Formula
   sha256 "c961870dbcc7c180467e3bbf546d3cef7b621eb02b8be96edd04c60ff72ff6c0"
   license "BSD-3-Clause"
 
-  # depends_on "cmake" => :build
+  depends_on "cmake" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "make", ".", *std_cmake_args
-    system "make", "radiosh64", *std_cmake_args
+    system "make", "radiosh64"
+    bin.install "radiosh"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test radhiSH`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "radiosh"
   end
 end
